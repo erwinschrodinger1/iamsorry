1c7a879fb7885b5ebc5aa09821d78c294a8bac7f
erwinschrodinger1 <81609547+erwinschrodinger1@users.noreply.github.com>
2023-05-25 00:34:10 +0545

    Adding config file format

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "<YOUR_API_KEY>",
+  authDomain: "<YOUR_AUTH_DOMAIN>",
+  projectId: "<YOUR_PROJECT_ID>",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+  measurementId: "...",
+};
